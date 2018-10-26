325aa688ae93c451995a98af839325bf74ee6cf1
Sai Yandapalli <saiyandapalli07@berkeley.edu>
2018-10-26 14:26:16 -0700

    working

---
+++ app/src/main/java/com/demo/mdb/spring2017finalassessment/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.demo.mdb.spring2017finalassessment;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        /* TODO Part 1
+         * Implement login. When the login button is pressed, attempt to login the user, and if
+         * that is successful, go to the TabbedActivity. If the register button is pressed, go to
+         * the RegisterActivity. Check the layout files for the IDs of the views used in this part
+         */
+
+
+    }
+}
